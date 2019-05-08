44c3b50dd8fe4eff242c803e5ea48495011feaa5
Lizzie <lchan217@gmail.com>
2019-05-08 17:10:30 -0500

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,12 +1,12 @@
 class SecretsController < ApplicationController
   before_action :require_login
-  def show 
+  def show
   end
 
   private
   def require_login
     unless session.include? :name
-      redirect_to :login
+      redirect_to '/login'
     end
   end
 end
