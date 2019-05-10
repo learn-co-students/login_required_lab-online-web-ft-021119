02d4257f8a4d7b470a7a9a31a54c524c6dd9ef00
github username <github email address>
2019-05-10 02:50:01 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # ApplicationController
+  # current_user
+  #   returns the name of the current user (FAILED - 1)
+  #   returns nil if nobody is logged in (FAILED - 2)
+
+  def current_user
+    session[:name]
+  end
 end
